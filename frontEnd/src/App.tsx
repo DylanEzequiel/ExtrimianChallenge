8cf51ea4d54052566b1f9e253379b28602d6210d
dylan correa <D.correa.E05@gmail.com>
2024-07-16 17:53:03 -0300

    Project Created

---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <h1 className='text-red-400'>Hola</h1>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
